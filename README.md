d87332418838feeaaface13f99301b80d6e83743
Toshiyuki Masui <masui@pitecan.com>
2022-07-13 14:54:00 +0900

    ndmacroの説明を追加

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
 <h1>again.el</h1>
 
 <ul>
-  <li><code>again.el</code>はEmacs上の操作を再実行するシステムです</li>
-  <li><a href="https://scrapbox.io/masui/Dynamic_Macro">Dynamic Macro</a>と異なり、同じ操作を二度繰り返す必要がありません</li>
+  <li><code>again.el</code>はEmacs上のさまざまな操作を再実行するシステムです</li>
+  <li><a href="https://scrapbox.io/masui/Dynamic_Macro">Dynamic Macro</a>と組み合わせて使います</li>
 </ul>
 
 <h2>使い方</h2>
@@ -11,6 +11,7 @@
   <li>Emacs上で「再実行キー」を押すと、最近実行したキー操作が再実行されます</li>
   <li>「最近実行されたキー操作」とは、1秒間キー操作が無かった時点から現在までのキー操作です</li>
   <li>1秒間キー操作をしなかった後で「abc」と入力し、すかさず「再実行キー」を押すと再度「abc」が入力されます</li>
+  <li>同じ操作を二度繰り返した後で「再実行キー」を押すと、繰り返されたキー操作が再実行されます</li>
 </ul>
 
 <h2>利用例</h2>
@@ -27,12 +28,13 @@
 <h2>インストール</h2>
 
 <ul>
-  <li><code>again.el</code>をEmacsのライブラリのパスに置き、
+  <li><code>again.el</code>と<code>ndmacro.el</code>をEmacsのライブラリのパスに置き、
     以下を<code>~/.emacs</code>に記述します</li>
-  <li>これはCtrl-Lを再実行キーに指定した例です</li>
+  <li>これはCtrl-Tを再実行キーに指定した例です</li>
   <pre>
+   (require 'ndmacro)
    (require 'again)
-   (defconst *again-key* "\C-l" "再実行キー") ;; Ctrl-Lを再実行キーにする場合
+   (defconst *again-key* "\C-t" "再実行キー") ;; Ctrl-tを再実行キーにする場合
    (global-set-key *again-key* 'exec-again)  </pre>
 </ul>
 
@@ -43,4 +45,5 @@
   <li>ユーザが何かを操作してから<code>*again-key*</code>を押すと、そのときの操作履歴を<code>*new-history*</code>という変数に格納します</li>
   <li><code>*new-history*</code>と<code>*old-history*</code>の差分が新しい操作だと考えられるので、これをキーボードマクロとして登録して実行します</li>
   <li>再度<code>*again-key*</code>が押されたときは、以前登録したマクロを再実行します</li>
+  <li>同じ操作が二度繰り返されていたときはDynamic Macroと同じ動作をします</li>
 </ul>
--- again.el
+++ again.el
@@ -3,25 +3,24 @@
 ;;
 ;;  増井俊之
 ;;  masui@masui.org
-;;  2022/7/12
+;;  2022/7/13
 ;;  https://github.com/masui/Again
 ;;
 
 ;;
 ;; ~/.emacsで以下を設定
 ;; (require 'again)
-;; (defconst *again-key* "\C-l" "再実行キー")
+;; (require 'ndmacro) ;; これも必要
+;; (defconst *again-key* "\C-t" "再実行キー")
 ;; (global-set-key *again-key* 'exec-again)
 ;;
 
-;;  M-x toggle-debug-on-error
-
 (provide 'again)
 
-(defvar *again-macro*    []  "繰り返し文字列")
-(defvar *old-history*    []  "ちょっと前のrecent-keys")
-(defvar *recent-history* []  "最新のrecent-keys")
-(defvar *dmacro-running* nil "dmacro実行中")
+(defvar *again-macro*       []  "繰り返し文字列")
+(defvar *old-history*       []  "ちょっと前のrecent-keys")
+(defvar *recent-history*    []  "最新のrecent-keys")
+(defvar *dmacro-is-running* nil "dmacro実行中")
 
 (defun clear-kbd-macro ()
   (setq *again-macro* [])
@@ -44,7 +43,7 @@
 	(setq found (equal s p))
 	(if found (setq res (substring s2 (- i))))
 	)
-      (setq i (1+ i))
+      (incf i)
       )
     res
     )
@@ -53,40 +52,6 @@
 (defun chomp (s) ; 文字列の最後の文字を除く
   (let ((len (length s)))
     (if (= len 0) []  (substring s 0 (1- len)))
-    )
-  )
-
-
-(defun exec-again () ;;; *again-key* で呼ばれる
-  (interactive)
-  (let* ((recent (recent-keys))
-	 (len (length recent))
-	 (again-key-repeated ;; *again-key* が連打されたかどうか
-	  (and (= (aref recent (- len 1)) (aref *again-key* 0))
-	       (= (aref recent (- len 2)) (aref *again-key* 0))))
-	 )
-    ;;
-    ;; 繰り返しがあるか、dmacro実行中に連打なら (ndmacro) を呼ぶ
-    ;;
-    (if (or (and *dmacro-running* again-key-repeated)
-	    (and (> (key-repeated) 1) (not again-key-repeated)))
-	(progn
-	  (ndmacro)
-	  (setq *dmacro-running* t)
-	  )
-      (setq *dmacro-running* nil)
-      (if (not again-key-repeated)
-	  (progn
-	    (if (not (equal *again-macro* [])) ; 新規作成じゃない場合
-		(setq *old-history* *recent-history*)
-	      )
-	    (setq *again-macro* (chomp (get-postfix *old-history* recent)))
-	    )
-	)
-      (setq *recent-history* recent)
-      ;;(setq xxx *again-macro*)
-      (execute-kbd-macro *again-macro*)
-      )
     )
   )
 
@@ -129,8 +94,39 @@
 ;; *recent-history*         abcLLLdeL
 ;; *again-macro*                  de    引算+実行
 
+(defun exec-again () ;;; *again-key* で呼ばれる
+  (interactive)
+  (let* ((recent (recent-keys))
+	 (len (length recent))
+	 (again-key-repeated ;; *again-key* が連打されたかどうか
+	  (and (= (aref recent (- len 1)) (aref *again-key* 0))
+	       (= (aref recent (- len 2)) (aref *again-key* 0))))
+	 )
+    ;;
+    ;; 繰り返しがあるか、dmacro実行中に連打なら (ndmacro) を呼ぶ
+    ;;
+    (if (or (and *dmacro-is-running* again-key-repeated)
+	    (and (> (key-repeated?) 1) (not again-key-repeated)))
+	(progn
+	  (ndmacro) ;; dmacro実行
+	  (setq *dmacro-is-running* t)
+	  )
+      (setq *dmacro-is-running* nil)
+      (if (not again-key-repeated)
+	  (progn
+	    (if (not (equal *again-macro* [])) ; 新規作成じゃない場合
+		(setq *old-history* *recent-history*)
+	      )
+	    (setq *again-macro* (chomp (get-postfix *old-history* recent)))
+	    )
+	)
+      (setq *recent-history* recent)
+      (execute-kbd-macro *again-macro*)
+      )
+    )
+  )
 
-(defun key-repeated ()
+(defun key-repeated? () ;; *again-key* が押される前にキー操作の繰り返しがあるか
   (let* ((a (recent-keys))
 	 (b (copy-sequence a))
 	 (i 0)
@@ -139,11 +135,11 @@
       (aset b (1+ i) (aref a i))
       (incf i)
       )
-    (repeated b)
+    (array-end-repeated? b)
     )
   )
 
-(defun repeated (array)
+(defun array-end-repeated? (array) ;; 配列の末尾に繰り返しがあるか
   (let (
 	(a (reverse array))
 	(alen (length array))
@@ -156,8 +152,6 @@
 	    )
 	(while (and matched (< j i))
 	  (if (not (= (aref a j) (aref a (+ i j))))
-
-
 	      (setq matched nil))
 	  (incf j)
 	  )
@@ -170,6 +164,7 @@
   )
 
 ;; (defconst *dmacro-key* "\C-l" "繰返し指定キー")
+
 
 
 
